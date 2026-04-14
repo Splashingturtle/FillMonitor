4924d7c5c3da54c718f16c3424cc0fcb87f7c4f8
汹涌灼烧 <2034408276@qq.com>
2026-04-14 20:33:28 +0800

    初始化项目结构，写完定时器

---
+++ SmartFillMonitor/SmartFillMonitor/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SmartFillMonitor.ViewModels;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SmartFillMonitor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public IServiceProvider ServiceProvider { get; private set; }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var services = new ServiceCollection();//创建DI集合
+            ConfigureServices(services);//把所有ViewModel放进这个集合
+            ServiceProvider = services.BuildServiceProvider();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+        }
+        #region DI
+        private void ConfigureServices(IServiceCollection service)
+        {
+            service.AddSingleton<AlarmsViewModel>();
+            service.AddSingleton<DashBoardViewModel>();
+            service.AddSingleton<DashQueryViewModel>();
+            service.AddSingleton<LogsViewModel>();
+            service.AddSingleton<SettingViewModel>();
+            service.AddSingleton<MainWindowViewModel>();
+        }
+        #endregion
+
+    }
+}
---
+++ SmartFillMonitor/SmartFillMonitor/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+
+namespace SmartFillMonitor
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        private readonly DispatcherTimer _timer;
+
+        [ObservableProperty]
+        private string currentTime;
+
+        public MainWindowViewModel()
+        {
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            _timer.Tick += Timer_Tick;
+            _timer.Start();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            CurrentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+    }
+}
