061c8bd084774e41b5f77f4f42aeea51151ce8ba
汹涌灼烧 <2034408276@qq.com>
2026-04-18 21:33:22 +0800

    用户控件和radiobutton跳转

--- SmartFillMonitor/SmartFillMonitor/MainWindowViewModel.cs
+++ SmartFillMonitor/SmartFillMonitor/MainWindowViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.DependencyInjection;
+using SmartFillMonitor.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,13 +14,18 @@ namespace SmartFillMonitor
 {
     public partial class MainWindowViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private object currentView;
+        private readonly IServiceProvider _serviceProvider;
+
         private readonly DispatcherTimer _timer;
 
         [ObservableProperty]
         private string currentTime;
 
-        public MainWindowViewModel()
+        public MainWindowViewModel(IServiceProvider serviceProvider)
         {
+            _serviceProvider = serviceProvider;
             _timer = new DispatcherTimer
             {
                 Interval = TimeSpan.FromSeconds(1)
@@ -30,5 +38,34 @@ namespace SmartFillMonitor
         {
             CurrentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
         }
+
+        #region Navigation Commands
+        [RelayCommand]  
+        private void NavigateToDashBoard()
+        {
+            CurrentView = _serviceProvider.GetRequiredService<DashBoardViewModel>();
+        }
+        [RelayCommand]
+        private void NavigateToDataQuery()
+        {
+            CurrentView = _serviceProvider.GetRequiredService<DashQueryViewModel>();
+        }
+        [RelayCommand]
+        private void NavigateToLogs()
+        {
+            CurrentView = _serviceProvider.GetRequiredService<LogsViewModel>();
+        }
+        [RelayCommand]
+        private void NavigateToAlarms()
+        {
+            CurrentView = _serviceProvider.GetRequiredService<AlarmsViewModel>();
+        }
+        [RelayCommand]
+        private void NavigateToSetting()
+        {
+            CurrentView = _serviceProvider.GetRequiredService<SettingViewModel>();
+        }
+
+        #endregion
     }
 }
--- SmartFillMonitor/SmartFillMonitor/ViewModels/AlarmsViewModel.cs
+++ SmartFillMonitor/SmartFillMonitor/ViewModels/AlarmsViewModel.cs
@@ -8,5 +8,6 @@ namespace SmartFillMonitor.ViewModels
 {
     class AlarmsViewModel
     {
+
     }
 }
